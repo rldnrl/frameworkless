ea0f4aeb0c79bc3aa9f460ee3cfd87042607cb1b
rldnrl <devpark1002@gmail.com>
2022-05-07 16:50:28 +0900

    feat: port 변경. MenuRouter 적용

--- moonbucks-menu-server/src/server.ts
+++ moonbucks-menu-server/src/server.ts
@@ -1,34 +1,27 @@
 import Koa from 'koa'
-import KoaLogger from "koa-logger";
-import bodyParser from "koa-bodyparser";
-import cors from "@koa/cors";
-import Router from "koa-router";
+import KoaLogger from 'koa-logger'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+import { MenuRouter } from './menu'
 
 const app = new Koa()
 
-const PORT = 3005
+const PORT = 4000
 
 app.use(bodyParser())
-app.use(cors({
-  origin: '*'
-}))
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 app.use(KoaLogger())
 
-const router = new Router()
-router.get('/api', async (ctx) => {
-  try {
-    ctx.body = {
-      status: 'success'
-    }
-  } catch (e) {
-    console.error(e)
-  }
-})
+app.use(MenuRouter.routes())
 
-app.use(router.routes())
-
-app.listen(PORT, async () => {
-  console.log(`Server listening http://localhost:${PORT}`)
-}).on('error', (err) => {
-  console.error(err)
-})
+app
+  .listen(PORT, async () => {
+    console.log(`Server listening http://localhost:${PORT}`)
+  })
+  .on('error', (err) => {
+    console.error(err)
+  })
