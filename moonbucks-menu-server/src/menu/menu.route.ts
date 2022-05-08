38abe687237660a3d383c9f54dc7c3b8448f90c0
rldnrl <devpark1002@gmail.com>
2022-05-08 15:23:27 +0900

    feat: add create menu route

--- moonbucks-menu-server/src/menu/menu.route.ts
+++ moonbucks-menu-server/src/menu/menu.route.ts
@@ -7,4 +7,6 @@ const menuMiddleware = new MenuMiddleware()
 
 menuRouter.get('/api/menus/:category', menuMiddleware.getMenusByCategory)
 
+menuRouter.post('/api/menus/:category', menuMiddleware.createMenusByCategory)
+
 export default menuRouter
