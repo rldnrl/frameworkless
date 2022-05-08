3b964d5bf8b57db049e231999856a33fff13448a
rldnrl <devpark1002@gmail.com>
2022-05-08 15:23:16 +0900

    feat: implement create menu logic

--- moonbucks-menu-server/src/menu/menu.middleware.ts
+++ moonbucks-menu-server/src/menu/menu.middleware.ts
@@ -1,13 +1,26 @@
-import { Context } from '@/types/context'
+import { RouterContext } from '@/types/context'
 import { mockMenus } from './mocks/mockMenu'
 
 export class MenuMiddleware {
-  async getMenusByCategory(ctx: Context) {
+  async getMenusByCategory(ctx: RouterContext) {
     const { category } = ctx.params
     try {
-      ctx.body = { [category]: mockMenus[category] }
+      ctx.body = {
+        [category]: mockMenus[category],
+      }
     } catch (error) {
       console.error(error)
+    }
+  }
+
+  async createMenusByCategory(ctx: RouterContext) {
+    const { category } = ctx.params
+    const { name, soldOut } = ctx.request.body
+    if (name && soldOut) {
+      mockMenus[category].push({ name, soldOut })
+      ctx.body = `Success Create ${category} menu`
+    } else {
+      ctx.body = '이름과 품절 여부를 입력해주세요.'
     }
   }
 }
